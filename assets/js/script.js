f954e078c7c22e765598f07a530bd09aa2747e12
bm <blake0marcus@gmail.com>
2022-04-10 22:47:08 -0700

    Check current hour againist time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,44 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").text(todayDate);
+
+
+$(function() {
+
+    //when save button is clicked, find value of text area and time block and save in localStorage
+    $(".saveBtn").on("click", function() {
+        var task = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, task);
+        console.log(task);
+        console.log(time);
+    })
+
+    var hourCheck = function() {
+        var currentHour = moment().hour();
+        console.log(currentHour);
+
+        $(".time-block").each(function () {
+            var hourBlock = parseInt($(this).attr("id"));
+            console.log(hourBlock);
+            if (hourBlock < currentHour) {
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+                $(this).addClass("past");
+
+            } else if (hourBlock === currentHour) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        });
+
+        
+    };
+    
+    hourCheck();
+});
